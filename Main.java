384514ff01d3ffbbc77de8916830cca66a0406b1
Thien1107 <69654098+Thien1107@users.noreply.github.com>
2020-08-13 15:13:27 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Thien");
+		System.out.println("I like listening to old 80s, 90s music beacuse they sound amazing");
+		System.out.println("I play badminton sometimes, but I'm really bad at it");
+		System.out.println("I'm still getting used to the new environment here so I might go quiet sometimes");
+		System.out.println("I'm looking forward to learning and experiencing this class with you this year!");
+	}
+}
